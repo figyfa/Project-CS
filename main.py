cb6409d0b13a5ab0df1a2987a6647d1ce99b0a15
josren0809 <josren0809@highgateschool.org.uk>
2025-04-30 13:01:33 +0100

    Added camera moving, next step is to let player "re_enter" bounding box

--- main.py
+++ main.py
@@ -29,10 +29,18 @@ class BoundingBox:
         self.height = height
         self.s = pygame.Surface((width,height))
         self.screen = screen
+
     def draw(self):
         self.s.set_alpha(128)
         self.s.fill((255,255,255))
         self.screen.blit(self.s,(self.lx,self.ty))
+
+    def scan_for_player(self,player):
+        if self.lx < player.cx - 5 and player.cx + 5 < (self.lx + self.width) and self.ty < player.cy - 5 and (self.lx + self.height) > player.cy + 5:
+            print("Player detected")
+            player.in_camera = True
+        else:
+            player.in_camera = False
 
 class Player:
     def __init__(self,cx,cy):
@@ -40,18 +48,44 @@ class Player:
         self.cy = cy
         self.vy = 0
         self.vx = 0
+        self.in_camera = True
     def draw(self):
-        pygame.draw.circle(screen, (0,0,0), (self.cx, self.cx),25)
-    def move(self,vx,vy):
-        self.vx = vx
-        self.vy = vy
-        self.cx += self.vx
-        self.cy += self.vy
+        pygame.draw.circle(screen, (0,0,0), (self.cx, self.cy),25)
+        #print("Player drawn at",self.cx,self.cy)
+
+class GameWorld:
+    def __init__(self,player):
+        self.objects = []
+        self.keys = pygame.key.get_pressed()
+        self.player = player
+
+    def move_camera(self,direction):
+        for item in self.objects:
+            if "down" in direction:
+                item.cy = item.cy - 3
+
+
+            if "up" in direction:
+                item.cy = item.cy + 3
+
+
+            if "left" in direction:
+                item.cx = item.cx + 3
+
+            if "right" in direction:
+                item.cx = item.cx - 3
+
+
+
 
 
 island = Island((0, 255, 60,50),500,400,300)
+
+
 camera_follow = BoundingBox()
 player = Player(400,300)
+world = GameWorld(player)
+world.objects.append(island)
 while running:
     screen.fill((107, 191, 255))
     island.draw()
@@ -60,11 +94,94 @@ while running:
     player.draw()
 
     keys = pygame.key.get_pressed()
+    world.keys = pygame.key.get_pressed()
+
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
             running = False
 
+    move_ticker = 0
+
+    if player.in_camera:
+        if keys[pygame.K_w]:
+            if move_ticker == 0:
+                move_ticker = 10
+                player.cy -= 3
+                #print("Moving up",player.cx,player.cy)
+                if keys[pygame.K_a]:
+                    player.cx -= 3
+                    #print("Moving up and left")
+                if keys[pygame.K_d]:
+                    player.cx += 3
+
+        if keys[pygame.K_s]:
+            if move_ticker == 0:
+                move_ticker = 10
+                player.cy += 3
+                #print("Moving down")
+                if keys[pygame.K_a]:
+                    player.cx -= 3
+                if keys[pygame.K_d]:
+                    #print("Moving down and right")
+                    player.cx += 3
+
+        if keys[pygame.K_a]:
+            if move_ticker == 0:
+                move_ticker = 10
+                player.cx -= 3
+                #print("moving left")
+
+        if keys[pygame.K_d]:
+            if move_ticker == 0:
+                move_ticker = 10
+                player.cx += 3
+                #print("moving right")
+
+    else:
+        if keys[pygame.K_w]:
+            if move_ticker == 0:
+                move_ticker = 10
+                world.move_camera(["up"])
+                if keys[pygame.K_a]:
+                    world.move_camera(["up", "left"])
+                if keys[pygame.K_d]:
+                    world.move_camera(["up", "right"])
+                if keys[pygame.K_s]:
+                    player.cy = player.cy + 1
+
+        if keys[pygame.K_s]:
+            if move_ticker == 0:
+                move_ticker = 10
+                world.move_camera(["down"])
+                if keys[pygame.K_a]:
+                    world.move_camera(["down", "left"])
+                if keys[pygame.K_d]:
+                    world.move_camera(["down", "right"])
+                if keys[pygame.K_w]:
+                    player.cy = player.cy - 1
+
+        if keys[pygame.K_a]:
+            if move_ticker == 0:
+                move_ticker = 10
+                world.move_camera(["left"])
+            if keys[pygame.K_d]:
+                player.cx = player.cx + 1
+
+        if keys[pygame.K_d]:
+            if move_ticker == 0:
+                move_ticker = 10
+                world.move_camera(["right"])
+            if keys[pygame.K_a]:
+                player.cx = player.cx - 1
+
+
+
+    if debugging:
+        if keys[pygame.K_UP]:
+            player.in_camera = False
+
+    camera_follow.scan_for_player(player)
 
     pygame.display.flip()
     fpsClock.tick(FPS)
